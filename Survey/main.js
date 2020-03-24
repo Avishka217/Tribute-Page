51ecec422bfd993883965574bc1bbfc34e169bdb
lordreigns <nisanabeywickrama@gmail.com>
2020-03-24 16:57:26 +0530

    Test

--- Survey/main.js
+++ Survey/main.js
@@ -7,25 +7,26 @@ var msg = document.createElement('div');
 var div = document.querySelector('.form-group');
 msg.id = 'message';
 myForm.insertBefore(msg, div);
-
-
-
-
-
 //form action
 myForm.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
     if (myName.value === '' || myEmail.value === '') {
+
         msg.classList.add('error');
         msg.innerHTML = "Please enter your name and email"
-
+        returnTop();
         setTimeout(() => msg.remove(), 2500);
+        reset();
 
     }
     else {
         myName.value = '';
         myEmail.value = '';
     }
+}
+// function to return to top of page 
+function returnTop() {
+    document.documentElement.scrollTop = 0;
 }
